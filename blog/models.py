f53fd0d3c6bf9ea26a5c4171460c80655b59b2a9
jhj1111 <happyijun@naver.com>
2025-07-30 17:47:43 +0900

    feat(models): create blog database class - Post

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import User
 from django.db import models
-
 # Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    create_date = models.DateTimeField()
+    
+    
+    def __str__(self):
+        return f'post title : {self.title}\ncontent : {self.content}'
+    
+    
+        

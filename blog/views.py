7d22c0f980322fd5efeffd9f2810ad4d10cdb0c7
jhj1111 <happyijun@naver.com>
2025-07-30 17:43:53 +0900

    feat(views): creat app views

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+def index(request):
+    posts_total_list = Post.objects.all() # db SELECT * FROM POST
+    
+    return render(request, 
+                  template_name='blog/index.html',
+                  context={'posts':posts_total_list},
+                  )
+    
+def detail(request, pk):
+    posts_page_list = Post.objects.get(pk=pk)
+    
+    return render(request,
+                  template_name='blog/detail.html',
+                  context={'post_page' : posts_page_list})
--- single_pages/views.py
+++ single_pages/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# function
+def landing(request):
+    
+    return render(request, 
+                  template_name='single_pages/landing.html'
+                  )

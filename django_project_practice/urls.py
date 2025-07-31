610455b198b7137c521d9fa064b9ae55486b98bc
jhj1111 <happyijun@naver.com>
2025-07-31 17:47:24 +0900

    feat(settings, urls): set media root, url

--- django_project_practice/urls.py
+++ django_project_practice/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django_project_practice import settings
 # http://127.0.0.1:8000/
 
 
@@ -32,3 +34,5 @@ urlpatterns = [
     path('blog/', include('blog.urls')),
     path('library/', include('library.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

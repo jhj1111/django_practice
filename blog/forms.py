d0b16a64cda70bc13da7c0ea79259503752f32aa
jhj1111 <happyijun@naver.com>
2025-07-31 20:41:42 +0900

    feat(forms): add fields uploaded_file

--- blog/forms.py
+++ blog/forms.py
@@ -2,6 +2,10 @@ from django import forms
 from .models import Post
 
 class PostForm(forms.ModelForm):
+    uploaded_file = forms.FileField(label='uploaded_file',
+                                    required=False,
+                                    widget=forms.FileInput(attrs={'class' : 'document_form'}),  # css class='document_from'
+                                    )
     class Meta: # class 설명
         model = Post
-        fields = ['title', 'content', 'uploaded_image']   # 속성값 입력, template, 오타 조심
+        fields = ['title', 'content', 'uploaded_image', 'uploaded_file']   # 속성값 입력, template, 오타 조심

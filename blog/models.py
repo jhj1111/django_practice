5ae1643f6d3b5f21d4fcf6e089bda3840c52db16
jhj1111 <happyijun@naver.com>
2025-07-31 20:42:46 +0900

    feat(models): add file db, create func get file_name/image_url

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+import os
 # Create your models here.
 class Post(models.Model):
     title = models.CharField(max_length=100)
@@ -7,13 +8,15 @@ class Post(models.Model):
     create_date = models.DateTimeField(auto_created=True, null=True)
     updated_date = models.DateTimeField(auto_now=True, null=True)
     uploaded_image = models.ImageField(upload_to='images/', null=True)
+    uploaded_file = models.FileField(upload_to='files/', null=True)
     
     def __str__(self):
-        try : image_path = self.uploaded_image.url
-        except ValueError : image_path = None
+        image_path = self.get_image_url()
+        file_name = self.get_file_name()
         
-        list_str = ['제목', '내용', '생성일', '변경일', '이미지 경로']
-        list_var = [self.title, self.content, self.create_date, self.updated_date, image_path]
+        
+        list_str = ['제목', '내용', '생성일', '변경일', '이미지 경로', '파일 경로']
+        list_var = [self.title, self.content, self.create_date, self.updated_date, image_path, file_name]
         
         # return f'post title : {self.title}\ncontent : {self.content} updated : {self.updated_date}'
         return '\n'.join([f'{i} : {j}' for i,j in zip(list_str, list_var)])
@@ -22,4 +25,14 @@ class Post(models.Model):
         return f'/blog/{self.pk}/'  # return url 지정
         # return reverse("model_detail", kwargs={"pk": self.pk})
     
+    def get_file_name(self):
+        try : file_path = os.path.basename(self.uploaded_file.name)
+        except (ValueError, TypeError) : file_path = None
+        
+        return file_path
     
+    def get_image_url(self):
+        try : image_url = self.uploaded_image.url
+        except ValueError : image_url = None
+        
+        return image_url
